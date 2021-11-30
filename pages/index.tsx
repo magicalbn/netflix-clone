e05f82fa3f5ce635b0d592ca4fb2ec5007515785
Nehal Bhandari <nehalvbhandari@gmail.com>
2021-11-30 15:54:52 +0530

    localised image files

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,9 +105,9 @@ const Home: React.FC<AppProps> = (props) => {
             
             <div className='login'>
 
-                <img className='background' src='https://assets.nflxext.com/ffe/siteui/vlv3/5dd45df7-33f1-4274-97ea-e9c6aca69dad/ed1cb962-411d-4e43-bad2-33f07ae8e341/IN-en-20211108-popsignuptwoweeks-perspective_alpha_website_medium.jpg'></img>
+                <img className='background' src='/website_large.jpg'></img>
                 <div className='head'>
-                    <img className='logo' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'></img>
+                    <img className='logo' src='/NavLogo.png'></img>
                     <button className='sign_in' onClick={(e) => setmodel(true)}>Sign In</button>
                 </div>
                 {model ? loginModel : content}
