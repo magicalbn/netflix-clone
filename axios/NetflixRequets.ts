438716333fcce338c6f09274bd2d059da22e4181
Nehal Bhandari <nehalvbhandari@gmail.com>
2023-12-13 12:56:35 +0530

    refactor

--- axios/NetflixRequets.ts
+++ axios/NetflixRequets.ts
@@ -1,15 +1,12 @@
-const API_KEY = 'fcb194a23dd01dfa41c515e04b4f8a44'
-
 export const request = {
-	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-	fetchNetflixOrignals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
-	fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-	fecthActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=25`,
-	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-	
-}
+    fetchTrending: `/trending/all/week?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`,
+    fetchNetflixOrignals: `/discover/tv?api_key=${process.env.NEXT_PUBLIC_API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`,
+    fecthActionMovies: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&with_genres=25`,
+    fetchHorrorMovies: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&with_genres=99`,
+};
 
-export const imageURL = 'http://image.tmdb.org/t/p/original'
+export const imageURL = "http://image.tmdb.org/t/p/original";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,140 +1,182 @@
-import { useState } from "react"
-import Layout from "@/components/Layout"
-import { IoIosArrowForward } from 'react-icons/io'
+import { useState } from "react";
+import Layout from "@/components/Layout";
+import { IoIosArrowForward } from "react-icons/io";
 
-import { ErrorCodes } from "firebaseErrorCodes"
-import panelData from '../data/panelData.json'
+import { ErrorCodes } from "firebaseErrorCodes";
+import panelData from "../data/panelData.json";
 //redux
-import { connect } from 'react-redux'
-import { UserActionList, ReducerStoreState, } from '@/store/actions'
+import { connect } from "react-redux";
+import { UserActionList, ReducerStoreState } from "@/store/actions";
 
-import Panel from "@/components/Rows/Panel"
-import FAQ from '@/components/FAQ'
-import Spinner from "@/components/UI/Spinner"
-import Fade from 'react-reveal/Fade';
+import Panel from "@/components/Rows/Panel";
+import FAQ from "@/components/FAQ";
+import Spinner from "@/components/UI/Spinner";
+import { Fade } from "react-awesome-reveal";
 interface AppProps {
-    initializeAuth: (email: string, password: string, login: boolean) => {},
+    initializeAuth: (email: string, password: string, login: boolean) => {};
 
-    AuthData: any
-
+    AuthData: any;
 }
 
 const Home: React.FC<AppProps> = (props) => {
-
-    const {AuthData,initializeAuth} = props
-    const [model, setmodel] = useState(false)
-    const [email, setEmail] = useState<string>('')
-    const [password, setpassword] = useState<string>('')
+    const { AuthData, initializeAuth } = props;
+    const [model, setmodel] = useState(false);
+    const [email, setEmail] = useState<string>("");
+    const [password, setpassword] = useState<string>("");
 
     const modelHandler = (e) => {
-        e.preventDefault()
-        setmodel(true)
-    }
+        e.preventDefault();
+        setmodel(true);
+    };
 
     const LogingHandler = (e) => {
-        e.preventDefault()
-        initializeAuth(email, password, true)
-    }
+        e.preventDefault();
+        initializeAuth(email, password, true);
+    };
 
     const demoLogin = () => {
-        console.log("demo")
-        initializeAuth("sample@sam.com","sample@123", true)
-    }
+        console.log("demo");
+        initializeAuth("sample@sam.com", "sample@123", true);
+    };
 
-    const SignUp = () => {       
-        initializeAuth(email, password, false)
-    }
-
-
+    const SignUp = () => {
+        initializeAuth(email, password, false);
+    };
 
     const content = (
         <>
-            <div className='content'>
+            <div className="content">
                 <h1>Unlimited movies, TV shows and more.</h1>
                 <h3>Watch anywhere. Cancel anytime.</h3>
-                <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                <h3>
+                    Ready to watch? Enter your email to create or restart your
+                    membership.
+                </h3>
                 <form onSubmit={modelHandler}>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type='email' placeholder='Email address' />
-                    <button type='submit'>Get Started <IoIosArrowForward /></button>
+                    <input
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        placeholder="Email address"
+                    />
+                    <button type="submit">
+                        Get Started <IoIosArrowForward />
+                    </button>
                 </form>
             </div>
-
         </>
-    )
+    );
 
     const panelViews = (
         <>
-        <div className="panel-collection">
-            {
-                panelData.map((each, index) => {
-                    return <Panel key={each.title} imgLink={each.imgLink} title={each.title} desc={each.desc} swap={index % 2 != 0} />
-                })
-            }
-
-        </div>
-        <FAQ/>
+            <div className="panel-collection">
+                {panelData.map((each, index) => {
+                    return (
+                        <Panel
+                            key={each.title}
+                            imgLink={each.imgLink}
+                            title={each.title}
+                            desc={each.desc}
+                            swap={index % 2 != 0}
+                        />
+                    );
+                })}
+            </div>
+            <FAQ />
         </>
-    )
+    );
 
     const loginModel = (
         <>
             <div className="login-model">
                 <Fade>
-                <h1>Sign In<p onClick={() => setmodel(false)}>x</p></h1>
+                    <h1>
+                        Sign In<p onClick={() => setmodel(false)}>x</p>
+                    </h1>
 
-                <form onSubmit={LogingHandler}>
-                    <input className='email' type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <input className='password' type='password' placeholder='Password' value={password} onChange={(e) => setpassword(e.target.value)} />
-                    {AuthData.error ? <p className="error">{ErrorCodes[AuthData.error.code]}</p> : null}
-                    <button className={AuthData.loading?'disable':''} type='submit'>{AuthData.loading?<Spinner/>:'Sign In'}</button>
-                    {!AuthData.loading && <button onClick={demoLogin} className="demo-login" >Demo login</button>}
-                    <div className="lower">
-                        <div className='checkbox'>
-                            <input type="checkbox" name="remember" value="remember" />
-                            <label>Remember me</label>
+                    <form onSubmit={LogingHandler}>
+                        <input
+                            className="email"
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            className="password"
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setpassword(e.target.value)}
+                        />
+                        {AuthData.error ? (
+                            <p className="error">
+                                {ErrorCodes[AuthData.error.code]}
+                            </p>
+                        ) : null}
+                        <button
+                            className={AuthData.loading ? "disable" : ""}
+                            type="submit"
+                        >
+                            {AuthData.loading ? <Spinner /> : "Sign In"}
+                        </button>
+                        {!AuthData.loading && (
+                            <button onClick={demoLogin} className="demo-login">
+                                Demo login
+                            </button>
+                        )}
+                        <div className="lower">
+                            <div className="checkbox">
+                                <input
+                                    type="checkbox"
+                                    name="remember"
+                                    value="remember"
+                                />
+                                <label>Remember me</label>
+                            </div>
+                            <p>Need help?</p>
                         </div>
-                        <p>Need help?</p>
-                    </div>
-
-                </form>
-                <p className="new">New to Netflix? <span className={AuthData.loading?'disable':''} onClick={() => SignUp()}>Sign up now</span></p>
+                    </form>
+                    <p className="new">
+                        New to Netflix?{" "}
+                        <span
+                            className={AuthData.loading ? "disable" : ""}
+                            onClick={() => SignUp()}
+                        >
+                            Sign up now
+                        </span>
+                    </p>
                 </Fade>
-
-
             </div>
-
         </>
-    )
+    );
     return (
         <Layout title="Netflix India" home>
-            
-            <div className='login'>
-
-                <img className='background' src='/website_large.jpg'></img>
-                <div className='head'>
-                    <img className='logo' src='/NavLogo.png'></img>
-                    <button className='sign_in' onClick={(e) => setmodel(true)}>Sign In</button>
+            <div className="login">
+                <img className="background" src="/website_large.jpg"></img>
+                <div className="head">
+                    <img className="logo" src="/NavLogo.png"></img>
+                    <button className="sign_in" onClick={(e) => setmodel(true)}>
+                        Sign In
+                    </button>
                 </div>
                 {model ? loginModel : content}
-
             </div>
-            {!model?panelViews:null}
+            {!model ? panelViews : null}
         </Layout>
-    )
-}
-
+    );
+};
 
 const mapStatetoProps = (state: ReducerStoreState) => {
     return {
-        AuthData: state.Authdata
-    }
-}
-
+        AuthData: state.Authdata,
+    };
+};
 
 const mapDispacthtoProps = (dispatch: any) => {
     return {
-        initializeAuth: (email, password, login) => dispatch(UserActionList.InitializeAuth(email, password, login)),
-
-    }
-}
-export default connect(mapStatetoProps, mapDispacthtoProps)(Home)
+        initializeAuth: (email, password, login) =>
+            dispatch(UserActionList.InitializeAuth(email, password, login)),
+    };
+};
+export default connect(mapStatetoProps, mapDispacthtoProps)(Home);
